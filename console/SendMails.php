345179411628ee1f6a8b6eec41eef801be83fd2b
Martin <skydiver@users.noreply.github.com>
2017-09-09 00:57:58 -0300

    * changed date format * sending emails

--- console/SendMails.php
+++ console/SendMails.php
@@ -2,17 +2,30 @@
 
 namespace Martin\Birthdays\Console;
 
+use DB;
+use Mail;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
+use RainLab\User\Models\User as UserModel;
 
 class SendMails extends Command {
 
     protected $name = 'birthdays:sendmails';
-    protected $description = 'Check for birthdats and send emails';
+    protected $description = 'Check for birthdays and send emails';
 
     public function fire() {
-        $this->output->writeln('Hello world!');
+
+        $birthdays = UserModel::where(DB::raw('MONTH(birthday)'), DB::raw('MONTH(NOW())'))
+            ->where(DB::raw('DAY(birthday)'), DB::raw('DAY(NOW())'))
+            ->get();
+
+        foreach ($birthdays as $user) {
+            Mail::send('martin.birthdays::mail.birthday', [$user], function ($message) use ($user) {
+                $message->to($user->email, $user->name);
+            });
+        }
+
     }
 
     protected function getArguments() {
